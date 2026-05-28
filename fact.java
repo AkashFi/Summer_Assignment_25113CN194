6dbc135fac28c43778eae36762c2acfe31e58534
AkashFi <akashshekhar912@gmail.com>
2026-05-28 16:29:33 +0530

    Add files via upload
    Add Day 1 summer Assignment

---
+++ fact.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class fact {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = input.nextInt();
+        int fact=1;
+
+        for(int i=1; i<=n; i++) {
+            fact=fact*i;
+        }
+        System.out.print( "Factorial of a number is : "+fact);
+    }
+}
