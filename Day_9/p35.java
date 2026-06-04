2a776b3a3c31493ab58ed7df2dcaf96e78d9c86f
AkashFi <akashshekhar912@gmail.com>
2026-06-04 19:49:33 +0530

    Day_9

---
+++ Day_9/p35.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class p35 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of rows : ");
+        int n = input.nextInt();
+
+        for(int i=0; i<n; i++) {
+            for(char ch='A'; ch<='A'+i; ch++) {
+                System.out.print((char)('A'+i));
+            }
+            System.out.println();
+        }
+        input.close();
+    } 
+}
